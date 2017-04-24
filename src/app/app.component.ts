52f4fd21d808e8d2cd456b2ec56e170e1ddfff77
Eliot Godard <Eliot Godard>
2017-04-24 17:20:49 +0200

    add redirection from sidenav to /lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	constructor(private router: Router) {}
+
+	onclick_lists() {
+		this.router.navigate(['/lists']);
+	}
 }
