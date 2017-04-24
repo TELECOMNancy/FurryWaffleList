14bb4259100b4012732e4febf14ef12b71aa66ce
Quentin TARDIVON <quentin.tardivon@gmail.com>
2017-04-24 09:27:00 +0200

    AngularFire init [ci skip]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAPMbmzPT5l8CvM0HgSeb6NAB1_J5jW0I8',
+  authDomain: 'furrywafflelist.firebaseapp.com',
+  databaseURL: 'https://furrywafflelist.firebaseio.com',
+  storageBucket: 'bucket.appspot.com'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
