3f2d291df8dc406ebcbe63889d5d172ed8fe9b0a
Yannick Philippe <yannick.philippe@telecomnancy.net>
2017-04-27 10:03:38 +0200

    edition sur element de list vote

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -203,10 +203,6 @@ export class ListComponent implements OnInit {
     })
   }
 
-  editElement(itemKey: string) {
-    console.log('Edition de ' + itemKey)
-  }
-
 export2CSV() {
     const data = []
     this.list.subscribe(list => {this.listName = list.name
